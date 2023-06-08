ec2c33c3b9189d3d3fffcd85887a4de72a30ccb2
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-08 16:28:42 +0200

    fix

--- deploy/01-deploy-token.ts
+++
@@ -1,8 +0,0 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
-import verify from "../utils/verify";
-import {
-    networkConfig,
-    developmentChains,
-    INITIAL_SUPPLY,
-} from "../helper-hardhat-config";
